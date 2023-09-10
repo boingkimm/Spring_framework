993f02734e683e9bb159768fe54f4e3ead832d6c
Kim Bohee <boingkimm@gmail.com>
2023-09-11 00:04:50 +0900

    빈 등록하지 않는 버전 - Component Scan

---
+++ Spring15_component_scan2/src/main/java/com/dao/DeptDAO.java
@@ -0,0 +1,24 @@
+package com.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+//@Component("dao")
+@Repository("dao")
+public class DeptDAO {
+
+	public List<String> list(){
+		return Arrays.asList("홍길동","이순신");
+	}
+
+	public DeptDAO() {
+		System.out.println("DeptDAO 생성자");
+	}
+	
+	
+}
