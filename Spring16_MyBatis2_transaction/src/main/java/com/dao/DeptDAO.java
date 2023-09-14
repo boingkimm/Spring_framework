0905c5ec1564ca1905928fa56cfc3ead68136e84
Kim Bohee <boingkimm@gmail.com>
2023-09-14 23:36:02 +0900

    Spring의 트랜잭션 처리

---
+++ Spring16_MyBatis2_transaction/src/main/java/com/dao/DeptDAO.java
@@ -0,0 +1,39 @@
+package com.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.dto.DeptDTO;
+
+@Repository("deptDAO")
+public class DeptDAO {
+	
+	@Autowired
+	SqlSessionTemplate session;
+	
+	//목록보기
+	public List<DeptDTO> deptList() {
+		List<DeptDTO> list = session.selectList("DeptMapper.deptList");
+		return list;
+	}
+
+	//
+	public int deptAdd(DeptDTO dto) {
+		int n = session.insert("DeptMapper.deptAdd", dto);
+		return n;
+	}
+	
+	public int deptUpdate(DeptDTO dto) {
+		int n = session.update("DeptMapper.deptUpdate", dto);
+		return n;
+	}
+	
+	public int deptDelete(int deptno) {
+		int n = session.delete("DeptMapper.deptDelete", deptno);
+		return n;
+	}
+
+}
